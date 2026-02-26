27083aea34dc5b7a43a5cf60079e3e92923fe318
Anand Raj <anadraj73073@gmail.com>
2026-02-27 00:41:06 +0530

    exclude langchain dpendency as turbopack don't bundle this

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+;const nextConfig: NextConfig = {
+  serverExternalPackages: [
+    "@langchain/community",
+    "@langchain/core",
+    "@langchain/qdrant",
+    "langchain",
+  ],
 };
 
 export default nextConfig;
+
